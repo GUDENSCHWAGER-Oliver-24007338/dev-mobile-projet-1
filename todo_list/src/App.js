ec3c097b2dc41c84668986b18f107b46db49fd74
Oliver_Gudenschwager <oliver.gudenschwager@etu.univ-amu.fr>
2026-03-27 17:22:24 +0100

    FEAT: More functionalities added

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -4,132 +4,230 @@ import data from "./data.json"
 import {useState} from "react";
 
 function App() {
-  const [mode, setMode] = useState("Tasks");
-  const [taches, setTaches] = useState(data.taches);
+    const [mode, setMode] = useState("Tasks");
+    const [taches, setTaches] = useState(data.taches);
+    const [relations, setRelations] = useState(data.relations);
+    const [dossiers, setDossiers] = useState(data.dossiers);
 
-  const [fNonFinies, setFNonFinies] = useState(true);
-  const [fDateCreation, setFDateCreation] = useState(false);
-  const [fDateEcheance, setFDateEcheance] = useState(false);
-  const [fNom, setFNom] = useState(false);
+    const [fNonFinies, setFNonFinies] = useState(true);
+    const [fDateCreation, setFDateCreation] = useState(false);
+    const [fDateEcheance, setFDateEcheance] = useState(true);
+    const [fNom, setFNom] = useState(false);
+    const [modeSimple, setModeSimple] = useState(true);
 
-  function LoadTasks() {
-      return (
-          <>
-              {taches && taches.map((task) =>
-                  <>
-                      <p>{task["title"]}</p>
-                      <p>ID: {task["id"]}</p>
-                      <p>Desc: {task["description"]}</p>
-                      <p>Created: {task["date_creation"]}</p>
-                      <p>Due by: {task["date_echeance"]}</p>
-                      <p>STATUS: {task["etat"]}</p>
-                      {task["equipiers"] && task["equipiers"].map((equipier) =>
-                          <>
-                              <p>Partner: {equipier["name"]}</p>
-                          </>
-                      )}
-                  </>
-              )}
-          </>
-    )
-  }
+    function LoadTasks() {
+        let tachesFiltrees = taches
 
-  function ResetTasks() {
-      const [BoolBtnClick, setBoolBtnClick] = useState(true);
-      const [BoolConfBtn, setBoolConfBtn] = useState(false);
+        if (fNonFinies) {
+            tachesFiltrees = taches.filter((task) => task["etat"] === "Nouveau" || task["etat"] === "En cours" || task["etat"] === "En attente");
+        }
 
-      function BtnClick() {
-          if (BoolBtnClick) {
-              setBoolBtnClick(false);
-              setBoolConfBtn(true);
-          } else {
-              setBoolBtnClick(true);
-              setBoolConfBtn(false);
-          }
-      }
+        function compareTachesDE(a,b) {
+            if (a["date_echeance"] < b["date_echeance"]){
+                return -1;
+            }
+            if (a["date_echeance"] > b["date_echeance"]){
+                return 1;
+            }
+            return 0;
+        }
+        function compareTachesDC(a,b) {
+            if (a["date_creation"] < b["date_creation"]){
+                return -1;
+            }
+            if (a["date_creation"] > b["date_creation"]){
+                return 1;
+            }
+            return 0;
+        }
+        function compareTachesT(a,b) {
+            if (a["title"] < b["title"]){
+                return -1;
+            }
+            if (a["title"] > b["title"]){
+                return 1;
+            }
+            return 0;
+        }
 
-      function Reset() {
-          setTaches("");
-          LoadTasks();
-          BtnClick();
-      }
+        if (fDateEcheance) {
+            tachesFiltrees.sort(compareTachesDE);
+        }
+        if (fDateCreation) {
+            tachesFiltrees.sort(compareTachesDC);
+        }
+        if (fNom) {
+            tachesFiltrees.sort(compareTachesT);
+        }
 
-      return (
-          <>
-            {BoolBtnClick && <button onClick={BtnClick}>Repartir de zéro</button>}
-            {BoolConfBtn && <button onClick={BtnClick}>Annuler</button>}
-            {BoolConfBtn && <button onClick={Reset}>Confirmer</button>}
-          </>
-      )
-  }
+        return (
+            <>
+                {tachesFiltrees && tachesFiltrees.map((task) =>
+                    <>
+                        <p>{task["title"]}</p>
+                        <p>ID: {task["id"]}</p>
+                        {task["description"] !== "" && <p>Desc: {task["description"]}</p>}
+                        <p>Created: {task["date_creation"]}</p>
+                        <p>Due by: {task["date_echeance"]}</p>
+                        <p>STATUS: {task["etat"]}</p>
+                        {task["equipiers"] && task["equipiers"].map((equipier) =>
+                            <>
+                                <p>Partner: {equipier["name"]}</p>
+                            </>
+                        )}
+                        <p>Categories:</p>
+                        {relations && relations.filter((relation) => relation["tache"] === task["id"]).filter((relation, order) => !modeSimple || order <= 1).map((relation) =>
+                            <>
+                                <p>{relation["dossier"]}</p>
+                            </>
+                        )}
+                        <p></p>
+                    </>
+                )}
+            </>
+        )
+    }
 
-  function CurrentMode() {
-      function ChangeMode() {
-          if (mode === "Tasks") {
-              setMode("Files")
-          } else {
-              setMode("Tasks")
-          }
-      }
+    function ResetTasks() {
+        const [BoolBtnClick, setBoolBtnClick] = useState(true);
+        const [BoolConfBtn, setBoolConfBtn] = useState(false);
 
-      return <button onClick={ChangeMode}>MODE: {mode}</button>
-  }
+        function BtnClick() {
+            if (BoolBtnClick) {
+                setBoolBtnClick(false);
+                setBoolConfBtn(true);
+            } else {
+                setBoolBtnClick(true);
+                setBoolConfBtn(false);
+            }
+        }
 
-  function LoadFilters() {
-      function ChangeFNonFinies() {
-          if (fNonFinies) {
-              setFNonFinies(false)
-          } else {
-              setFNonFinies(true)
-          }
-      }
+        function Reset() {
+            setTaches("");
+            LoadTasks();
+            BtnClick();
+        }
 
-      function ChangeFDateCreation() {
-          if (fDateCreation) {
-              setFDateCreation(false)
-          } else {
-              setFDateCreation(true)
-          }
-      }
+        return (
+            <>
+                {BoolBtnClick && <button onClick={BtnClick}>Reset</button>}
+                {BoolConfBtn && <button onClick={BtnClick}>Cancel</button>}
+                {BoolConfBtn && <button onClick={Reset}>Confirm</button>}
+            </>
+        )
+    }
 
-      function ChangeFDateEcheance() {
-          if (fDateEcheance) {
-              setFDateEcheance(false)
-          } else {
-              setFDateEcheance(true)
-          }
-      }
+    function CurrentMode() {
+        function ChangeMode() {
+            if (mode === "Tasks") {
+                setMode("Files")
+            } else {
+                setMode("Tasks")
+            }
+        }
 
-      function ChangeFNom() {
-          if (fNom) {
-              setFNom(false)
-          } else {
-              setFNom(true)
-          }
-      }
+        return <button onClick={ChangeMode}>Mode: {mode}</button>
+        }
 
-      return (
-          <>
-              <p>Filter/Order by</p>
-              <button onClick={ChangeFNonFinies}>Unfinished {fNonFinies && "✓"}</button>
-              <button onClick={ChangeFDateCreation}>Creation Date {fDateCreation && "✓"}</button>
-              <button onClick={ChangeFDateEcheance}>Due Date {fDateEcheance && "✓"}</button>
-              <button onClick={ChangeFNom}>Name {fNom && "✓"}</button>
-          </>
-      )
-  }
+        function LoadFilters() {
+            function ChangeFNonFinies() {
+                if (fNonFinies) {
+                    setFNonFinies(false)
+                } else {
+                    setFNonFinies(true)
+                }
+            }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <CurrentMode />
-          {mode === "Tasks" && <ResetTasks />}
-          {mode === "Tasks" && <LoadFilters />}
-          {mode === "Tasks" && <LoadTasks />}
-              </header>
-    </div>
-  );
+        function ChangeFDateCreation() {
+            if (fDateCreation) {
+                setFDateCreation(false)
+            } else {
+                setFDateCreation(true)
+                setFDateEcheance(false)
+                setFNom(false)
+            }
+        }
+
+        function ChangeFDateEcheance() {
+            if (fDateEcheance) {
+                setFDateEcheance(false)
+            } else {
+                setFDateEcheance(true)
+                setFNom(false)
+                setFDateCreation(false)
+            }
+        }
+
+        function ChangeFNom() {
+            if (fNom) {
+                setFNom(false)
+            } else {
+                setFNom(true)
+                setFDateCreation(false)
+                setFDateEcheance(false)
+            }
+        }
+
+        return (
+            <>
+                <p>Filter/Order by</p>
+                <button onClick={ChangeFNonFinies}>Unfinished {fNonFinies && "✓"}</button>
+                <button onClick={ChangeFDateCreation}>Creation Date {fDateCreation && "✓"}</button>
+                <button onClick={ChangeFDateEcheance}>Due Date {fDateEcheance && "✓"}</button>
+                <button onClick={ChangeFNom}>Name {fNom && "✓"}</button>
+            </>
+        )
+    }
+
+    function LoadFiles() {
+        return (
+            <>
+                {dossiers && dossiers.map((dossier) =>
+                    <>
+                        <p>{dossier["title"]}</p>
+                        {dossier["icon"] !== "" && <p>Icon: {dossier["icon"]}</p>}
+                        <p>ID: {dossier["id"]}</p>
+                        {dossier["type"] !== "" && <p>Type: {dossier["type"]}</p>}
+                        {dossier["description"] !== "" && <p>Desc: {dossier["description"]}</p>}
+                        <p>Color: {dossier["color"]}</p>
+                        <p></p>
+                    </>
+                )}
+            </>
+        )
+    }
+
+    function LoadMode() {
+
+        function ChangeMode() {
+            if (modeSimple) {
+                setModeSimple(false)
+            } else {
+                setModeSimple(true)
+            }
+        }
+
+        return (
+            <>
+                <button onClick={ChangeMode}>△</button>
+                <p>Mode: {modeSimple ? "Simple" : "Complet"}</p>
+            </>
+        )
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <LoadMode />
+                <CurrentMode />
+                {mode === "Tasks" && <ResetTasks />}
+                {mode === "Tasks" && <LoadFilters />}
+                {mode === "Tasks" && <LoadTasks />}
+                {mode === "Files" && <LoadFiles />}
+            </header>
+        </div>
+    );
 }
 
 export default App;
